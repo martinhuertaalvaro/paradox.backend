2d4efce7c65e2bf555fae27e021ff569f315a84d
Alvaro <martinhuertaalvaro@gmail.com>
2024-05-10 20:50:00 +0200

    #1 sql

--- consultas.sql
+++ consultas.sql
@@ -8,3 +8,4 @@ WHERE id = 6;
 
 INSERT INTO BBDD.TABLA ( //campos ) VALUES ( //valores );
 INSERT INTO bbddparadox.tenant ( name,code,active ) VALUES ( 'PARADOX' ,'paradox',true);
+INSERT INTO bbddparadox.user ( tenant_id_id,email,roles,active,password,name,surname,birthdate,city,workfield,registerdate) VALUES ( 1 ,'amartinh@paradox.com','["ROLE_ADMIN"]',true,'$2y$13$PomIFMs7SSFNBPbESW3oZevUL90GTvxwZXOanT7gpnLtVZr4aEyDK','Álvaro','Martín','2004/11/24','Valencia','Developer','2024/04/04');
